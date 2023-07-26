e2e557a54988d346f4bad0ad2d596b3dd3ca4b7e
Ravi MAlav <ravimalav05@gmail.com>
2023-07-26 18:25:40 +0530

    added .gitignore file

--- util/database.js
+++ util/database.js
@@ -1,9 +1,9 @@
 const Sequelize=require('sequelize')
 const process=require('process')
-const sequelize=new Sequelize('daytoday_expenses','root',process.env.DATABASE_PASS,
+const sequelize=new Sequelize('daytoday_expenses',process.env.DATABASE_USERNAME,process.env.DATABASE_PASS,
 {
-    dialect:'mysql',
-    host:'localhost'
+    dialect:process.env.DATABASE_DIALECTNAME,
+    host:process.env.DATABASE_HOSTNAME
 })
 
 module.exports=sequelize;
