3fc08e0cd6e6f995294cdf5c161187e987fac1f1
Ravi MAlav <ravimalav05@gmail.com>
2023-07-27 18:19:55 +0530

    added .env file to backendreadyBranch

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize=require('sequelize')
 const process=require('process')
 const sequelize=new Sequelize(process.env.DATABASE_NAME,process.env.DATABASE_USERNAME,process.env.DATABASE_PASS,
 {
-    dialect:'mysql',
+    dialect:process.env.DATABASE_DIALECTNAME,
     host:process.env.DATABASE_HOSTNAME
 })
 
