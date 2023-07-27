b74c5beea342801e2f5574b48692efe71f4ee0bf
Ravi MAlav <ravimalav05@gmail.com>
2023-07-27 14:44:16 +0530

    added dialect expicitly

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize=require('sequelize')
 const process=require('process')
-const sequelize=new Sequelize('daytoday_expenses',process.env.DATABASE_USERNAME,process.env.DATABASE_PASS,
+const sequelize=new Sequelize(process.env.DATABASE_NAME,process.env.DATABASE_USERNAME,process.env.DATABASE_PASS,
 {
-    dialect:process.env.DATABASE_DIALECTNAME,
+    dialect:'mysql',
     host:process.env.DATABASE_HOSTNAME
 })
 
